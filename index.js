12ba78c81632dff0f7ec1b8da84dacc34119b591
tcmbonani <tcmbonani@gmail.com>
2023-11-20 21:04:14 +0200

    profile

---
+++ index.js
@@ -0,0 +1,8 @@
+int amount = 9;
+
+switch(amount) {
+    case     0 : System.out.println("amount is  0"); break;
+    case     5 : System.out.println("amount is  5"); break;
+    case    10 : System.out.println("amount is 10"); break;
+    default    : System.out.println("amount is something else");
+}
